041929b6a11cd08b04ff93dd79bc1899124f56f2
Vivek9744 <125950773+Vivek9744@users.noreply.github.com>
2023-10-10 12:50:26 +0530

    updated show more

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,32 +1,38 @@
-import React, { useState ,useEffect} from 'react';
-import { useParams } from 'react-router-dom';
-import {moviesRef} from '../firebase/firebase';
-import {doc, getDoc} from 'firebase/firestore';
-import {db} from '../firebase/firebase';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { moviesRef } from "../firebase/firebase";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../firebase/firebase";
+import { ThreeCircles } from "react-loader-spinner";
 const Detail = () => {
-  const {id}=useParams();
-  const[data,setData]=useState({
-    title:"",
-    year:"",
-    image:"",
-    description:""
+  const { id } = useParams();
+  const [data, setData] = useState({
+    title: "",
+    year: "",
+    image: "",
+    description: "",
   });
-  useEffect(()=>{
-     async function getData(){
-     const _doc= doc(db,"movies",id);
-     const _data=await getDoc(_doc);
-     setData(_data.data());
+
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    async function getData() {
+      setLoading(true);
+      const _doc = doc(db, "movies", id);
+      const _data = await getDoc(_doc);
+      setData(_data.data());
+      setLoading(false); // Set loading to false when data is fetched
     }
     getData();
-  },[])
-  const [showMore, setShowMore]=useState(false);
-//  const {text}=data.description;
+  }, []);
+
+  const [showMore, setShowMore] = useState(false);
+
+  //  const {text}=data.description;
   const [showDescription, setShowDescription] = useState(true);
   const [showReviews, setShowReviews] = useState(false);
   const [showDetails, setShowDetails] = useState(false);
   // Function to show Description section
   const showDescriptionSection = () => {
-    
     setShowDescription(true);
     setShowReviews(false);
     setShowDetails(false);
@@ -45,13 +51,121 @@ const Detail = () => {
   };
   return (
     <div>
-    {   console.log(data)}
+      {/* {   console.log(data)} */}
+
       <section className="text-gray-600 body-font overflow-hidden">
-        <div className="container px-5 py-24 mx-auto">
-          <div className="lg:w-4/5 mx-auto flex flex-wrap">
-            {/* Image section for smaller devices */}
-            <div className="w-full lg:hidden mb-4">
-              <div className="w-full h-80 object-cover object-center rounded">
+        {loading ? (
+          <div className="h-96 flex w-full justify-center items-center">
+            <ThreeCircles height={30} color="white" />
+          </div>
+        ) : (
+          <div className="container px-5 py-24 mx-auto">
+            <div className="lg:w-4/5 mx-auto flex flex-wrap">
+              {/* Image section for smaller devices */}
+              <div className="w-full lg:hidden mb-4">
+                <div className="w-full h-80 object-cover object-center rounded">
+                  <img
+                    alt="ecommerce"
+                    className="w-full h-full"
+                    src={data.image}
+                  />
+                </div>
+              </div>
+              <div className="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
+                <h1 className="text-gray-100 text-3xl title-font font-medium mb-4 ">
+                  {data.title}
+                </h1>
+                <h2 className="text-gray-100 text-3xl title-font font-medium mb-4">
+                  {data.year}
+                </h2>
+                <div className="flex mb-4">
+                  <a
+                    className={`flex-grow ${
+                      showDescription
+                        ? "text-indigo-500 border-b-2 border-indigo-500"
+                        : "border-b-2 border-gray-300"
+                    } py-2 text-lg px-1 cursor-pointer`}
+                    onClick={showDescriptionSection}
+                  >
+                    Description
+                  </a>
+                  <a
+                    className={`flex-grow ${
+                      showReviews
+                        ? "text-indigo-500 border-b-2 border-indigo-500"
+                        : "border-b-2 border-gray-300"
+                    } py-2 text-lg px-1 cursor-pointer`}
+                    onClick={showReviewsSection}
+                  >
+                    Reviews
+                  </a>
+                  <a
+                    className={`flex-grow ${
+                      showDetails
+                        ? "text-indigo-500 border-b-2 border-indigo-500"
+                        : "border-b-2 border-gray-300"
+                    } py-2 text-lg px-1 cursor-pointer`}
+                    onClick={showDetailsSection}
+                  >
+                    Details
+                  </a>
+                </div>
+                {/* Description Section */}
+                {showDescription && (
+  <div className="leading-relaxed mb-4">
+    {data.description.length <= 250 || showMore ? (
+      data.description
+    ) : (
+      <>
+        {data.description.substring(0, 250)}
+        <button
+          className="btn text-orange-700"
+          onClick={() => setShowMore(!showMore)}
+        >
+          {showMore ? "Show less" : "Show more"}
+        </button>
+      </>
+    )}
+  </div>
+)}
+
+
+
+                {/* Reviews Section */}
+                {showReviews && <div>vivek chaudhary is hero</div>}
+                {/* Details Section */}
+                {showDetails && (
+                  <div>{/* Add your Details content here */}</div>
+                )}
+                <div className="flex">
+                  <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
+                    Button
+                  </button>
+                  <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
+                    <svg
+                      fill="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      className="w-5 h-5"
+                      viewBox="0 0 24 24"
+                    >
+                      {/* SVG path */}
+                      <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z" />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+              {/* Image section for larger devices */}
+              <div
+                className="hidden lg:block lg:w-1/2 h-80 object-cover object-center rounded"
+                style={{
+                  position: "fixed",
+                  top: "10",
+                  right: "10px",
+                  zIndex: "999",
+                }}
+              >
                 <img
                   alt="ecommerce"
                   className="w-full h-full"
@@ -59,102 +173,10 @@ const Detail = () => {
                 />
               </div>
             </div>
-            <div className="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
-             
-              <h1 className="text-gray-100 text-3xl title-font font-medium mb-4">
-                {data.title}
-              </h1>
-              <h2 className="text-gray-100 text-3xl title-font font-medium mb-4">
-                {data.year}
-              </h2>
-              <div className="flex mb-4">
-                <a
-                  className={`flex-grow ${
-                    showDescription
-                      ? 'text-indigo-500 border-b-2 border-indigo-500'
-                      : 'border-b-2 border-gray-300'
-                  } py-2 text-lg px-1 cursor-pointer`}
-                  onClick={showDescriptionSection}
-                >
-                  Description
-                </a>
-                <a
-                  className={`flex-grow ${
-                    showReviews
-                      ? 'text-indigo-500 border-b-2 border-indigo-500'
-                      : 'border-b-2 border-gray-300'
-                  } py-2 text-lg px-1 cursor-pointer`}
-                  onClick={showReviewsSection}
-                >
-                  Reviews
-                </a>
-                <a
-                  className={`flex-grow ${
-                    showDetails
-                      ? 'text-indigo-500 border-b-2 border-indigo-500'
-                      : 'border-b-2 border-gray-300'
-                  } py-2 text-lg px-1 cursor-pointer`}
-                  onClick={showDetailsSection}
-                >
-                  Details
-                </a>
-              </div>
-              {/* Description Section */}
-              {showDescription && (
-                <p className="leading-relaxed mb-4">
-                  
-                  <h6>
-                      {showMore ? data.description : `${data.description.substring(0, 250)}`}
-                      <button className="btn text-orange-700" onClick={() => setShowMore(!showMore)}>
-                      {showMore ? "  Show less" : "  Show more"}</button>
-                      </h6>
-                  {/* Rest of the description content */}
-                </p>
-              )}
-              {/* Reviews Section */}
-              {showReviews && (
-                <div>
-                  vivek chaudhary is hero
-                </div>
-              )}
-              {/* Details Section */}
-              {showDetails && (
-                <div>
-                  {/* Add your Details content here */}
-                </div>
-              )}
-              <div className="flex">
-                <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
-                  Button
-                </button>
-                <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
-                  <svg
-                    fill="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    className="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    {/* SVG path */}
-                    <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z" />
-                  </svg>
-                </button>
-              </div>
-            </div>
-            {/* Image section for larger devices */}
-            <div className="hidden lg:block lg:w-1/2 h-80 object-cover object-center rounded" style={{ position: 'fixed', top: '10', right: '10px', zIndex: '999' }}>
-              <img
-                alt="ecommerce"
-                className="w-full h-full"
-                src={data.image}
-              />
-            </div>
           </div>
-        </div>
+        )}
       </section>
     </div>
-    
   );
 };
 export default Detail;
