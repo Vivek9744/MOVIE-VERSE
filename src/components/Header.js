715f3f7cc82f23100978f29b69d8675e6f35fbac
Vivek9744 <125950773+Vivek9744@users.noreply.github.com>
2023-10-15 23:19:12 +0530

    login page created

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,18 @@
 import AddIcon from '@mui/icons-material/Add';
 import Button from '@mui/material/Button';
-import {Appstate}from '../App'
+import React,{useContext} from 'react';
+import LoginSharpIcon from '@mui/icons-material/LoginSharp';
 // import AddMovie from './AddMovie'
 import {Link } from 'react-router-dom';
+import {Appstate} from '../App'
 // import { red } from '@mui/material/colors';
 const Header = () => {
-  const useAppstate=useContext(Appstate);
-   
+  const useAppstate=useContext(Appstate); 
   return (
     <div className='text-3xl flex justify-between items-center text-red-500 font-bold p-3 border-b-2 border-gray-500 sticky top-0  z-10 bg-black'>
       <Link to='/'><span>Filmy<span className='text-white'>Verse</span></span></Link>
-     { 
-      useAppstate.login?<Link to="/addmovie">
+      { useAppstate.login?
+      <Link to="/addmovie">
       <h1 className="text-lg text-white cursor-pointer flex items-center">
       
       <Button variant="">
@@ -19,15 +20,20 @@ const Header = () => {
       Add New
     </Button>
   </h1>
-</Link>:  <Link to="/addmovie">
+</Link>
+:
+<Link to="/login">
       <h1 className="text-lg text-white cursor-pointer flex items-center">
       
       <Button variant="">
-      <AddIcon className="mr-1" style={{ color: 'red' }} />
-      Add New
+      <LoginSharpIcon className="mr-1" style={{ color: 'red' }} />
+      Login
     </Button>
   </h1>
-</Link>}}
+</Link>
+      }
+
+
       <Link to="/search">
       <h1 className="text-lg text-white cursor-pointer flex items-center">
       
